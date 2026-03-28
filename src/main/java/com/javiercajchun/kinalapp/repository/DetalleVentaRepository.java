40f8e2f27d166672a4fa67977fd078259e2dd92a
jcajchun-2023512 <jcajchun-2023512@kinal.edu.gt>
2026-03-27 23:43:04 -0600

    feat: agrego repositorio DetalleVentaRepository

---
+++ src/main/java/com/javiercajchun/kinalapp/repository/DetalleVentaRepository.java
@@ -0,0 +1,7 @@
+package com.javiercajchun.kinalapp.repository;
+
+import com.javiercajchun.kinalapp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+}
