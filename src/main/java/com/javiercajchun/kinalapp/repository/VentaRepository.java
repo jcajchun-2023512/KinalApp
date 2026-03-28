ff92e3ea63ba47c27d79ca5f7d559230cd2ea20b
jcajchun-2023512 <jcajchun-2023512@kinal.edu.gt>
2026-03-27 22:12:16 -0600

    fix: correcciones en el repositorio VentaRepository de la aplicación KinalApp

--- src/main/java/com/javiercajchun/kinalapp/repository/VentaRepository.java
+++ src/main/java/com/javiercajchun/kinalapp/repository/VentaRepository.java
@@ -2,6 +2,8 @@ package com.javiercajchun.kinalapp.repository;
 
 import com.javiercajchun.kinalapp.entity.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-public interface VentaRepository extends JpaRepository<Venta, Integer> {
+public interface VentaRepository extends JpaRepository<Venta, Long> {
 }
