d6d76eeedeb8518cf6e880653ce2903a11d0febd
jcajchun-2023512 <jcajchun-2023512@kinal.edu.gt>
2026-03-27 22:09:59 -0600

    fix: correcciones en el repositoro ProductoRepository de la aplicación KinalApp

--- src/main/java/com/javiercajchun/kinalapp/repository/ClienteRepository.java
+++ src/main/java/com/javiercajchun/kinalapp/repository/ClienteRepository.java
@@ -2,8 +2,7 @@ package com.javiercajchun.kinalapp.repository;
 
 import com.javiercajchun.kinalapp.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
 public interface ClienteRepository extends JpaRepository<Cliente,String> {
 
